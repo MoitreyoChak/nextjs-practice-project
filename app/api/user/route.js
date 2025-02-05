9fb35e2f9dc48a1457d06c8076fe8185a8f755b2
Moitreyo <cmoitreyo@gmail.com>
2025-02-05 15:19:51 +0530

    Added prisma and connected Postgres

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,6 +1,30 @@
-export function GET() {
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
+const client = new PrismaClient();
+
+export function GET() {
     return Response.json({
         name: "Shelby"
+    })
+}
+
+export async function POST(req) {
+    //extract the body
+    const body = await req.json();
+    console.log(body);
+
+    //save in db
+    const user = await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    })
+    console.log("user id: ", user.id);
+
+    //send response
+    return NextResponse.json({
+        message: "Successfully logged in"
     })
 }
